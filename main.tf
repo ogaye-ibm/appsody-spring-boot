083f11bb92f6058038000100e1ddf7a086468a0f
Omar Gaye <56206659+ogaye-ibm@users.noreply.github.com>
2020-03-21 20:08:07 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,10 @@ resource "ibm_resource_key" "cloudant_key" {
   role                 = local.role
   resource_instance_id = ibm_resource_instance.cloudant_instance.id
 
+  depends_on = [
+    ibm_resource_instance.cloudant_instance,
+  ]
+  
   //User can increase timeouts
   timeouts {
     create = "150m"
@@ -47,9 +51,14 @@ resource "ibm_container_bind_service" "cloudant_binding" {
   resource_group_id     = data.ibm_resource_group.tools_resource_group.id
   key                   = ibm_resource_key.cloudant_key.name
 
+  depends_on = [
+    ibm_resource_key.cloudant_key,
+  ]
+  
   // The provider (v16.1) is incorrectly registering that these values change each time,
   // this may be removed in the future if this is fixed.
   lifecycle {
     ignore_changes = [id, namespace_id, service_instance_name]
   }
 }
+
